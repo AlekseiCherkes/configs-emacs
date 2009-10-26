6bdd9b14d81450dfa4dea66f167aa5f9a7430a18
U-CHERKES\Administrator <Administrator@Cherk.(none)>
2009-10-26 19:42:35 +0200

    Base functionality added.

---
+++ my-basics.el
@@ -0,0 +1,178 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Базовые настройки (не требуют установки дополнительных пкетов)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Корректная работа с UTF-8 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(set-language-environment "UTF-8")
+
+;(setq file-name-coding-system 'utf-8)
+
+(setq file-coding-system-alist '(("" . utf-8) ))
+(setq process-coding-system-alist '(("" . utf-8) ))
+(setq network-coding-system-alist '(("" . utf-8) ))
+(setq gnus-group-charset-alist '((".*" utf-8)))
+
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(set-clipboard-coding-system 'utf-8)
+(set-language-environment-coding-systems 'utf-8)
+
+(prefer-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+
+; Нужно для корректной вставки русских букв из других программ 
+; через буффер обмена. Как работает не знаю.
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; EFFECTIVE EMACS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Убираем все ненужное с экрана
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+; Не помню зачем
+(put 'scroll-left 'disabled nil)
+
+;; Отвечаем y/n вместо yes/no.
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-xm" 'execute-extended-command)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Перемещение между окнами с помощью Alt + Up/Down/Left/Right
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'windmove)
+(windmove-default-keybindings 'meta)
+
+(setq win-step 10)
+
+(defun win-resize-top-or-bot ()
+  "Figure out if the current window is on top, bottom or in the middle"
+  (let* ((win-edges (window-edges))
+	 (this-window-y-min (nth 1 win-edges))
+	 (this-window-y-max (nth 3 win-edges))
+	 (fr-height (frame-height)))
+    (cond
+     ((eq 0 this-window-y-min) "top")
+     ((eq (- fr-height 1) this-window-y-max) "bot")
+     (t "mid"))))
+
+(defun win-resize-left-or-right ()
+  "Figure out if the current window is to the left, right or in the middle"
+  (let* ((win-edges (window-edges))
+	 (this-window-x-min (nth 0 win-edges))
+	 (this-window-x-max (nth 2 win-edges))
+	 (fr-width (frame-width)))
+    (cond
+     ((eq 0 this-window-x-min) "left")
+     ((eq (+ fr-width 4) this-window-x-max) "right")
+     (t "mid"))))
+
+(defun win-resize-enlarge-horiz ()
+  (interactive)
+  (cond
+   ((equal "top" (win-resize-top-or-bot)) (enlarge-window (- win-step)))
+   ((equal "bot" (win-resize-top-or-bot)) (enlarge-window (+ win-step)))
+   ((equal "mid" (win-resize-top-or-bot)) (enlarge-window (- win-step)))
+   (t (message "nil"))))
+
+(defun win-resize-minimize-horiz ()
+  (interactive)
+  (cond
+   ((equal "top" (win-resize-top-or-bot)) (enlarge-window (+ win-step)))
+   ((equal "bot" (win-resize-top-or-bot)) (enlarge-window (- win-step)))
+   ((equal "mid" (win-resize-top-or-bot)) (enlarge-window (+ win-step)))
+   (t (message "nil"))))
+
+(defun win-resize-enlarge-vert ()
+  (interactive)
+  (cond
+   ((equal "left"  (win-resize-left-or-right)) (enlarge-window-horizontally (+ win-step)))
+   ((equal "right" (win-resize-left-or-right)) (enlarge-window-horizontally (- win-step)))
+   ((equal "mid"   (win-resize-left-or-right)) (enlarge-window-horizontally (+ win-step)))
+   ))
+
+(defun win-resize-minimize-vert ()
+  (interactive)
+  (cond
+   ((equal "left"  (win-resize-left-or-right)) (enlarge-window-horizontally (- win-step)))
+   ((equal "right" (win-resize-left-or-right)) (enlarge-window-horizontally (+ win-step)))
+   ((equal "mid"   (win-resize-left-or-right)) (enlarge-window-horizontally (- win-step)))
+   ))
+
+(global-set-key [C-M-down] 'win-resize-minimize-horiz)
+(global-set-key [C-M-up] 'win-resize-enlarge-horiz)
+(global-set-key [C-M-left] 'win-resize-minimize-vert)
+(global-set-key [C-M-right] 'win-resize-enlarge-vert)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Other
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(cua-mode)
+
+; IBuffer by default
+(global-set-key (kbd "C-x C-b") 'ibuffer-other-window)
+
+; Other stuff
+(setq tab-width 4)
+(setq transient-mark-mode t)
+(setq current-language-environment "UTF-8")
+(setq c-basic-offset 4)
+(setq show-trailing-whitespace t) 
+
+; Hot key for view buffer size
+;(global-set-key [\C-f8] '(buffer-size nil))
+
+; Подсветка строки на которой находится курсор
+; (highlight-current-line-on t)
+; (highlight-current-line-whole-line-on t)
+; (highlight-current-line-set-bg-color "grey25")
+
+;; Make backups go to a backup directory
+(add-to-list 'backup-directory-alist 
+	     '("." . "~/.emacs_backups"))
+
+; Показываем время.
+;(setq display-time-interval 1)
+;(setq display-time-format "%H:%M:%S")
+;(display-time-mode)
+
+; Переключалка буфферов
+(iswitchb-mode 1)
+;(ido-mode 1)
+
+;; Можно сохранять сессию перед выходом.
+;(desktop-save-mode t)
+
+;;(desktop-save-mode t)
+
+(global-set-key [\C-backspace] 'backward-kill-word)
+
+;; Lines wrap
+(setq default-truncate-lines t) ;; disable line wrap
+(setq truncate-partial-width-windows nil) ;; make side by side buffers function the same as the main window
+(global-set-key [f12] 'toggle-truncate-lines) ;; Add F12 to toggle line wrap
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Color Them
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;(require 'color-theme)
+;(color-theme-charcoal-black)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; FONTS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;(require 'zoom-frm')
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- my-emacs.el
+++ my-emacs.el
@@ -1,1 +1,9 @@
-(message "my-emacs loaded!!!")
+﻿; Устанавливаем файл для custom settings.
+(setq custom-file "~/emacs/my-custom.el")
+(load custom-file 'noerror)
+
+; Дирректории с elisp кодом.
+(setq load-path (cons "~/emacs/" load-path))
+
+; Загружаем остальные модули.
+(load "my-basics.el")
