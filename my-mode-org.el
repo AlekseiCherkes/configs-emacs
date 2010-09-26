736eaf408b8fb5bd7b8f188b9c4fbf5818dd27ba
aleksei.cherkes <aleksei.cherkes@gmail.com>
2010-09-26 18:50:09 +0300

    Some basic org-mode tuning.

--- my-mode-org.el
+++ my-mode-org.el
@@ -8,4 +8,19 @@
 (require 'org-install)
 (org-reload) ; we don't need default emacs version
 
+;; With this setup, all files with extension ‘.org’ 
+;; will be put into Org mode.
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode)) 
+
+;; not needed when global-font-lock-mode is on 
+;(add-hook 'org-mode-hook 'turn-on-font-lock) 
+
+;; The last three lines define global keys for some 
+;; commands — please choose suitable keys yourself.
+(global-set-key "\C-cl" 'org-store-link) 
+(global-set-key "\C-ca" 'org-agenda) 
+(global-set-key "\C-cb" 'org-iswitchb)
+
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
