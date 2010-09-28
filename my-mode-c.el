dca3fd6b710ef7e14edef07b75aa42710530669a
a_cherkes <aleksey.cherkes@gmail.com>
2010-09-28 13:14:35 +0300

    Little changes for more convinient editing HLSL files via c-mode.

--- my-mode-c.el
+++ my-mode-c.el
@@ -7,15 +7,19 @@
 (defun my-c-mode-common-setup ()
   (setq c-basic-offset 4) ;; We want to stick to KStars' indentation standards
   (setq indent-tabs-mode nil) ;; TABs for indentation, please!
+  (setq tab-width 4)
+  (setq tab-always-indent t)
+  (setq indent-tabs-mode t)
   ;(setq compile-command "makefromemacs") ;; Change compile command
   ;(define-key c-mode-base-map (kbd "C-c c") 'compile) ;; Bind C-c c to (compile)
   ;(setup-ebrowse-bindings) ;; Set up EBrowse bindings
+  (print "CMode handled")
 )
 
 (add-hook 'c-mode-common-hook 'my-c-mode-common-setup)
 
 ; Добавляем файлы шейдеров в c-mode
-(add-to-list 'auto-mode-alist '("\\.\\(fx\\|inc\\)\\'" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(fx\\|inc\\fxh\\)\\'" . c-mode))
 
 ;(add-to-list 'auto-mode-alist '("\\.[ch]\\(pp\\|xx\\|\\+\\+\\)\\'" . c++-mode))
 ;(add-to-list 'auto-mode-alist '("\\.\\(CC?\\|HH?\\)\\'" . c++-mode))
