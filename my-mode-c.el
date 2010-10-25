4b0ace7d4a69d9bf88c05dd116a6112a188fc633
a_cherkes <aleksey.cherkes@gmail.com>
2010-10-25 20:15:18 +0300

    C mode minor bug fixes.

--- my-mode-c.el
+++ my-mode-c.el
@@ -10,17 +10,16 @@
   (setq tab-width 4)
   (setq tab-always-indent t)
   (setq indent-tabs-mode t)
-  (c-subworld-mode)
+  (c-subword-mode)
   ;(setq compile-command "makefromemacs") ;; Change compile command
   ;(define-key c-mode-base-map (kbd "C-c c") 'compile) ;; Bind C-c c to (compile)
   ;(setup-ebrowse-bindings) ;; Set up EBrowse bindings
-  (print "CMode handled")
 )
 
 (add-hook 'c-mode-common-hook 'my-c-mode-common-setup)
 
 ; Добавляем файлы шейдеров в c-mode
-(add-to-list 'auto-mode-alist '("\\.\\(fx\\|inc\\fxh\\)\\'" . c-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(fx\\|inc\\|fxh\\)\\'" . c++-mode))
 
 ;(add-to-list 'auto-mode-alist '("\\.[ch]\\(pp\\|xx\\|\\+\\+\\)\\'" . c++-mode))
 ;(add-to-list 'auto-mode-alist '("\\.\\(CC?\\|HH?\\)\\'" . c++-mode))
